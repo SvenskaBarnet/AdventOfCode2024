7f86d2ab551169598aa7943ef938a50c220c403f
svenskabarnet <svenskabarnet@gmail.com>
2024-12-01 19:43:08 +0100

    refactor of input parsing

--- Day1/Day1.cs
+++ Day1/Day1.cs
@@ -3,7 +3,7 @@ using System.Numerics;
 namespace aoc2024.Day1;
 public class Day1
 {
-    public static void Part1()
+    private static (List<int> left, List<int> right) ParseInput()
     {
         string[] input = File.ReadAllLines(Path.Combine("Day1", "input"));
         List<int> left = [];
@@ -29,12 +29,18 @@ public class Day1
             right.Add(int.Parse(rightNum));
         }
 
+        return (left, right);
+    }
+
+    public static void Part1()
+    {
+        (List<int> left, List<int> right) = ParseInput();
         left.Sort();
         right.Sort();
 
         BigInteger totalDistance = 0;  
 
-        for (int i = 0; i < input.Length; i++)
+        for (int i = 0; i < left.Count; i++)
         {
             totalDistance += Math.Abs(left[i]-right[i]);
         }
@@ -44,29 +50,7 @@ public class Day1
 
     public static void Part2()
     {
-        string[] input = File.ReadAllLines(Path.Combine("Day1", "input"));
-        List<int> left = [];
-        List<int> right = [];
-
-        foreach (var line in input)
-        {
-            string leftNum = string.Empty;
-            string rightNum = string.Empty;
-            for (int i = 0; i < line.Length; i++)
-            {
-                if (i < 5)
-                {
-                    leftNum += line[i];
-                }
-                else if (i > 7)
-                {
-                    rightNum += line[i];
-                }
-            }
-
-            left.Add(int.Parse(leftNum));
-            right.Add(int.Parse(rightNum));
-        }
+        (List<int> left, List<int> right) = ParseInput();
 
         int similarityScore = 0;
         foreach (int number in left)
